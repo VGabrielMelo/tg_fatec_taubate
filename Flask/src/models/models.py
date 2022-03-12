abde8aa7c1c630e419f724650be1193503813b74
NicholasRoque <nicholas.sroque@gmail.com>
2022-03-11 21:02:27 -0300

    a

---
+++ Flask/src/models/models.py
@@ -0,0 +1,14 @@
+from src.server.instance import server
+from flask_sqlalchemy import SQLAlchemy
+app = server.app
+db=SQLAlchemy(app)
+class UsuarioModel(db.Model):
+    __tablename__ ='usuarios'
+    id=db.Column(db.Integer(),primary_key=True)
+    nome=db.Column(db.String(200),nullable=False)
+    email=db.Column(db.String(50),nullable=False)
+    senha=db.Column(db.String(100),nullable=False)
+    def __init__(self,nome,email,senha):
+        self.nome=nome
+        self.email=email
+        self.senha=senha
