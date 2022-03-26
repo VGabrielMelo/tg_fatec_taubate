54e7cd47adf368b168430d9e27247be7d3f27d49
Victor Melo <60778206+VGabrielMelo@users.noreply.github.com>
2022-03-26 19:31:34 -0300

    Arrumando a lógica

--- Flask/src/utils/Scrapping.py
+++ Flask/src/utils/Scrapping.py
@@ -2,21 +2,23 @@ from pickle import UNICODE
 import bs4 as bs
 import urllib.request
 import re
+
 def scrapped(self,Empresa):
     Empresa = str('Cartão_private_label')
-    scraped_data = urllib.request.urlopen(f'https://pt.wikipedia.org/wiki/{Empresa}')
+    #Aqui vai a url para pegar o resumo da pesquisa:
+    URL = f""
+    scraped_data = urllib.request.urlopen(URL)
     article = scraped_data.read()
 
-parsed_article = bs.BeautifulSoup(article,'lxml')
-paragraphs = parsed_article.find_all('p')
+    parsed_article = bs.BeautifulSoup(article,'lxml')
+    paragraphs = parsed_article.find_all('p')
+    
+    article_text = ""
+    for p in paragraphs:
+        article_text += p.text
 
-article_text = ""
-
-for p in paragraphs:
-    article_text += p.text
-
-# Removendo sujeiras
-article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-article_text = re.sub(r'\s+', ' ', article_text)
-formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
-formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    # Removendo sujeiras
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+    article_text = re.sub(r'\s+', ' ', article_text)
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
