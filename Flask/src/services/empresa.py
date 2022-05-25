488b4aeef950c3e80f1fe34ba4b33bc658f2bd2e
NicholasRoque <nicholas.sroque@gmail.com>
2022-05-24 21:35:44 -0300

    add top comments

--- Flask/src/services/empresa.py
+++ Flask/src/services/empresa.py
@@ -38,7 +38,9 @@ class EmpresaService:
             response_indeed = {
                 'porcentagem_positivo':((len(processados_indeed['positivo'])/len(reviews_indeed['reviews']))*100),
                 'porcentagem_negativo':((len(processados_indeed['negativo'])/len(reviews_indeed['reviews']))*100),
-                'porcentagem_neutro':((len(processados_indeed['neutro'])/len(reviews_indeed['reviews']))*100)
+                'porcentagem_neutro':((len(processados_indeed['neutro'])/len(reviews_indeed['reviews']))*100),
+                'top_positivos': processados_indeed['positivo'][slice(-10,len(processados_indeed['positivo']))],
+                'top_negativos': processados_indeed['negativo'][slice(-10,len(processados_indeed['negativo']))]
             }
             return response_indeed
         except Exception as e:
